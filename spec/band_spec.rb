963ce16aaff04070d14851f27c2c01f2488620a4
Cory Spitzer <coryspitzer@gmail.com>
2015-05-15 14:41:38 -0700

    Test and call validation for the presence of a Band name

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,8 +2,9 @@ require('spec_helper')
 
 describe(Band) do
   it { should have_and_belong_to_many(:venues) }
-  # 
-  # it('') do
-  #
-  # end
+
+  it('validates the presence of a name') do
+    band = Band.new(genre: 'post-rock')
+    expect(band.save).to eq(false)
+  end
 end
